c1d41a7f4d4f38f1defab9f69c5bbe5ac646067f
Iago Flávio <iagoflavio_sa@hotmail.com>
2024-07-27 16:18:59 -0300

    Indo até a página de pessoas

--- lkdn_follow.py
+++ lkdn_follow.py
@@ -22,4 +22,22 @@ pag.click(lkdn_search_bar)
 pag.write('analista de dados')
 pag.press('enter')
 
-# Localizar o botão 'pessoas'
+time.sleep(5)
+
+# Localizar o botão 'pessoas' e clicar
+pessoas_btn = pag.locateOnScreen('images/pessoas_btn.png')
+pag.click(pessoas_btn)
+
+time.sleep(2)
+
+# Localizar o botão 'empresa atual' e clicar
+empresa_atual_btn = pag.locateOnScreen('images/empresa_atual_btn.png')
+pag.click(empresa_atual_btn)
+
+time.sleep(0.5)
+
+# Filtrar por empresa == 'Data Mundo'
+data_mundo_btn = pag.locateOnScreen('images/data_mundo_btn.png')
+pag.click(data_mundo_btn)
+exibir_resultados_btn = pag.locateOnScreen('images/exibir_resultados_btn.png')
+pag.click(exibir_resultados_btn)
