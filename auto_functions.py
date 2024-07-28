1dee82ec12b0c90d3955d072737b9dcd505de7fd
Iago Flávio <iagoflavio_sa@hotmail.com>
2024-07-28 12:13:07 -0300

    Implementado a função de buscar e conectar, e código organizado

---
+++ auto_functions.py
@@ -0,0 +1,40 @@
+import pyautogui as pag
+from time import sleep
+
+def connect(count):
+    """
+    count = Quantidade de vezes que o botão conectar será apertado
+    ------------------------------------------------------------
+    Procurar pelo botão "conectar" e o botão "avançar". Se o botão conectar não estiver na tela, procurar pelo botão avançar.
+    Se o botão avançar não estiver na tela ainda, rolar e repetir o processo.
+    """
+    n = 0
+    while True:
+        try:
+            conectar_btn = pag.locateOnScreen('images/conectar_btn.png')
+            print(conectar_btn)
+        except:
+            conectar_btn = None
+            print(f"conectar_btn = {conectar_btn}")
+        if conectar_btn is None:
+            try:
+                avancar_btn = pag.locateOnScreen('images/avancar_btn.png')
+                print(avancar_btn)
+            except:
+                avancar_btn = None
+                print(f"avançar_btn = {avancar_btn}")
+            if avancar_btn is None:
+                pag.scroll(-300)
+                continue
+            pag.click(avancar_btn)
+            sleep(2)
+            continue 
+        for button in list(pag.locateAllOnScreen('images/conectar_btn.png')):
+            print(f"button = {button}")
+            pag.click(button)
+            n += 1
+            if n == count:
+                return
+
+def click(img_name):
+    return pag.click(pag.locateOnScreen('images/' + img_name + '.png'))
--- lkdn_follow.py
+++ lkdn_follow.py
@@ -1,43 +1,45 @@
-import pyautogui as pag
-import time
+from pyautogui import write, press
+from time import sleep
+from auto_functions import click, connect
 
-pag.PAUSE = 0.3
+PAUSE = 0.3
 
 # Abrir o chrome
-pag.press('win')
-pag.write('chrome')
-pag.press('enter')
+press('win')
+write('chrome')
+press('enter')
 
-time.sleep(2)
+sleep(2)
 
 # Entrar no linkedin
-pag.write('br.linkedin.com')
-pag.press('enter')
+write('br.linkedin.com')
+press('enter')
 
-time.sleep(5)
+sleep(5)
 
-# Clicar na barra e pesquisa e digitar 'analista de dados', depoisd dar enter
-lkdn_search_bar = pag.locateOnScreen('images/lkdn_search_bar.png')
-pag.click(lkdn_search_bar)
-pag.write('analista de dados')
-pag.press('enter')
+# Clicar na barra de pesquisa e digitar 'analista de dados', depois dar enter
+click('lkdn_search_bar')
+write('analista de dados')
+press('enter')
 
-time.sleep(5)
+sleep(5)
 
 # Localizar o botão 'pessoas' e clicar
-pessoas_btn = pag.locateOnScreen('images/pessoas_btn.png')
-pag.click(pessoas_btn)
-
-time.sleep(2)
+click('pessoas_btn')
+sleep(2)
 
 # Localizar o botão 'empresa atual' e clicar
-empresa_atual_btn = pag.locateOnScreen('images/empresa_atual_btn.png')
-pag.click(empresa_atual_btn)
-
-time.sleep(0.5)
+click('empresa_atual_btn')
+sleep(0.5)
 
 # Filtrar por empresa == 'Data Mundo'
-data_mundo_btn = pag.locateOnScreen('images/data_mundo_btn.png')
-pag.click(data_mundo_btn)
-exibir_resultados_btn = pag.locateOnScreen('images/exibir_resultados_btn.png')
-pag.click(exibir_resultados_btn)
+click('data_mundo_btn')
+click('exibir_resultados_btn')
+
+sleep(2)
+
+'''
+Procurar pelo botão "conectar" e o botão "avançar". Se o botão conectar não estiver na tela, procurar pelo botão avançar.
+Se o botão avançar não estiver na tela ainda, rolar e repetir o processo.
+'''
+connect(15)
